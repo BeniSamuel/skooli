5678bdbe976460f9b5edaa41884871912f0dc40b
benisamuel <benisamuel566@gmail.com>
2025-11-04 13:28:46 +0200

    applying migrations

--- discipline/models.py
+++ discipline/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Discipline(models.Model):
+    mistake = models.CharField()
+    occured_at = models.DateTimeField()
+    deducted_marks = models.IntegerField()
+
+    class Meta:
+        pass
+
