49a9066cf51c43c215617ac0357f1f8489be8392
benisamuel <benisamuel566@gmail.com>
2025-11-06 21:15:42 +0200

    added tailwindcss

---
+++ theme/static_src/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "../templates/**/*.html",
+    "../../**/templates/**/*.html",
+    "./src/**/*.js",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
